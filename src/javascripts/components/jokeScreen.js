e29dda8f7be419517868f4b38ae68dd3cd506b2a
Jim Administrator <jamesdavidconner@gamil.com>
2021-03-24 22:18:20 -0500

    working on error related to getJokes

--- src/javascripts/components/jokeScreen.js
+++ src/javascripts/components/jokeScreen.js
@@ -1,15 +1,16 @@
-const buildJoke = (jokeArray) => {
-  jokeArray.forEach((item) => {
-    document.querySelector('#display-section').innerHTML += `
+const buildJoke = () => {
+  // jokeArray.forEach(() => {
+  document.querySelector('#display-section').innerHTML += `
       <div class="card text-center" id="jokeCard">
       <img class="card-img-top" src="/images/jokegenjs 2.png" alt="Card image cap"> 
-        <div class="card-body">
-          <h3>${item.setup}</h3>
-          <h3>${item.punchline}</h3>
-          <button id="joke-button--${item.id}" type="joke-button" class="btn btn-dark">GET A JOKE</button>
-        </div>
+        <button id="joke-button" type="joke-button" class="btn btn-dark">GET A JOKE</button>
       </div>`;
-  });
+  // });
 };
+
+/* <div class="card-body">
+<h3 id="setup">${item.setup}</h3>
+<h3 id="punchline">${item.punchline}</h3>
+</div> */
 
 export default buildJoke;
--- src/javascripts/events/domEvents.js
+++ src/javascripts/events/domEvents.js
@@ -1,10 +1,14 @@
-import getJokes from '../helpers/data/jokesData';
+// import getJokes from '../helpers/data/jokesData';
 
 const domEvents = () => {
   document.querySelector('body').addEventListener('click', (e) => {
     e.preventDefault();
     if (e.target.id.includes('joke-button')) {
-      getJokes();
+      // const jokeObject = {
+      //   jokeName: document.querySelector('#setup').value,
+      //   jokeImageURL: document.querySelector('#punchline').value
+      // };
+      // addRides(rideObject).then((ridesArray) => createRides(ridesArray));
     }
   });
 };
